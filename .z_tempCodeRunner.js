804bd1aa232a6f2e1da8337b53a316fc934dbe8b
Orlando <opseua@gmail.com>
2023-03-21 19:36:17 -0300

    Nova tentativa de deploy

--- .z_tempCodeRunner.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const WebSocket = require('ws');
-const cors = require('cors');
-
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 3333;
-console.log(port);
