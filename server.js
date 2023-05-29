3bf4dbd2bc83b03646ea9cf44fddfe5e2e9c1fc4
Orlando <opseua@gmail.com>
2023-05-29 07:57:02 -0300

    Código reduzido

--- server.js
+++ server.js
@@ -1,60 +1,26 @@
-const express = require('express');
-const WebSocket = require('ws');
-const cors = require('cors');
+const WebSocketServer = require('ws');
 
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 3333;
+const port = 8888;
+const wss = new WebSocketServer.Server({ port: port });
+const clients = new Set();
 
-const app = express();
-app.use(express.text());
-app.use(cors());
-
-const server = app.listen(port, () => {
-    console.log(`RODANDO NA PORTA ${port}`);
+wss.on("connection", ws => {
+    clients.add(ws);
+    console.log("NOVO CLIENTE");
+    ws.on("message", data => {
+        const message = data.toString();
+        clients.forEach(client => {
+            if (client !== ws) {
+                client.send(message);
+            }
+        });
+    });
+    ws.on("close", () => {
+        clients.delete(ws);
+        console.log("CLIENTE DESCONECTADO");
+    });
+    ws.onerror = function () {
+        console.log("ERRO");
+    };
 });
-
-const wss = new WebSocket.Server({ server });
-
-app.post('/enviar-dados', (req, res) => {
-    const dados = req.body;
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(dados);
-        }
-    });
-    app.emit('enviar-dados', dados);
-    res.send("RECEBIDO");
-});
-
-app.get('/receber-dados', (req, res) => {
-    res.set({
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
-    });
-
-    res.write(`SSE: CONECTADO\n`);
-    console.log('SSE: NOVA CONEXAO\n');
-
-    app.on('enviar-dados', (dados) => {
-        res.write(`${dados}\n`);
-    });
-
-    req.socket.on('close', () => {
-        console.log('SSE: SERVIDOR PAROU\n');
-    });
-});
-
-wss.on('connection', (ws) => {
-    ws.send('WEBSOCKET: CONECTADO\n');
-    console.log('WEBSOCKET: NOVA CONEXAO\n');
-
-    ws.on('message', (message) => {
-        app.emit('enviar-dados', message);
-    });
-
-    ws.on('close', () => {
-        console.log('WEBSOCKET: SERVIDOR PAROU\n');
-    });
-});
+console.log(`RODANDO NA PORTA: ${port}`)
