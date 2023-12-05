e80e21c89eb0d751a2a17e1b0eadd885c8a1434c
Orlando <opseua@gmail.com>
2023-12-05 03:36:52 -0300

    Padronização com ESLint

--- src/client.js
+++ src/client.js
@@ -2,9 +2,9 @@ let e = import.meta.url;
 async function client(inf) {
     let ret = { 'ret': false }; e = inf && inf.e ? inf.e : e;
     if (catchGlobal) {
-        const errs = async (err, ret) => { if (!ret.stop) { ret['stop'] = true; let retRegexE = await regexE({ 'e': err, 'inf': inf, 'catchGlobal': true }) } }
-        if (typeof window !== 'undefined') { window.addEventListener('error', (err) => errs(err, ret)); window.addEventListener('unhandledrejection', (err) => errs(err, ret)) }
-        else { process.on('uncaughtException', (err) => errs(err, ret)); process.on('unhandledRejection', (err) => errs(err, ret)) }
+        const errs = async (errC, ret) => { if (!ret.stop) { ret['stop'] = true; let retRegexE = await regexE({ 'e': errC, 'inf': inf, 'catchGlobal': true }) } }
+        if (typeof window !== 'undefined') { window.addEventListener('error', (errC) => errs(errC, ret)); window.addEventListener('unhandledrejection', (errC) => errs(errC, ret)) }
+        else { process.on('uncaughtException', (errC) => errs(errC, ret)); process.on('unhandledRejection', (errC) => errs(errC, ret)) }
     }
     try {
         let time = dateHour().res; console.log(`${time.day}/${time.mon} ${time.hou}:${time.min}:${time.sec}`, 'client [WebSocket]');
@@ -55,12 +55,12 @@ async function client(inf) {
             }
         }
 
-        async function keepCookieLiveRun() {
-            await new Promise(resolve => { setTimeout(resolve, 15000) });
-            wsSend({ 'e': e, 'url': devChrome, 'message': { 'other': 'keepCookieLive' } })
+        // async function keepCookieLiveRun() {
+        //     await new Promise(resolve => { setTimeout(resolve, 15000) });
+        //     wsSend({ 'e': e, 'url': devChrome, 'message': { 'other': 'keepCookieLive' } })
 
-        };
-        // keepCookieLiveRun();
+        // };
+        // // keepCookieLiveRun();
 
         ret['ret'] = true
         ret['msg'] = `CLIENT [Chrome_Extension]: OK`
--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ let e = import.meta.url;
 async function server(inf) {
     let ret = { 'ret': false }; e = inf && inf.e ? inf.e : e;
     if (catchGlobal) {
-        const errs = async (err, ret) => { if (!ret.stop) { ret['stop'] = true; let retRegexE = await regexE({ 'e': err, 'inf': inf, 'catchGlobal': true }) } }
-        if (typeof window !== 'undefined') { window.addEventListener('error', (err) => errs(err, ret)); window.addEventListener('unhandledrejection', (err) => errs(err, ret)) }
-        else { process.on('uncaughtException', (err) => errs(err, ret)); process.on('unhandledRejection', (err) => errs(err, ret)) }
+        const errs = async (errC, ret) => { if (!ret.stop) { ret['stop'] = true; let retRegexE = await regexE({ 'e': errC, 'inf': inf, 'catchGlobal': true }) } }
+        if (typeof window !== 'undefined') { window.addEventListener('error', (errC) => errs(errC, ret)); window.addEventListener('unhandledrejection', (errC) => errs(errC, ret)) }
+        else { process.on('uncaughtException', (errC) => errs(errC, ret)); process.on('unhandledRejection', (errC) => errs(errC, ret)) }
     }
     try {
         let infConfigStorage = { 'e': e, 'action': 'get', 'key': 'webSocket' };
@@ -137,8 +137,7 @@ async function server(inf) {
         let wss = new _WebSocketServer({ server });
         wss.on('connection', async (ws, req) => {
             ws.isAlive = true;
-            ws.on('error', async (text) => {
-                (console.error);
+            ws.on('error', async (error) => {
                 await log({ 'e': e, 'folder': 'JavaScript', 'path': `log.txt`, 'text': error })
             })
             let urlParts = req.url.split('/')
@@ -206,12 +205,12 @@ async function server(inf) {
             runFun1()
 
             // [URA_Reversa]
-            async function runFun2() {
-                if (letter !== 'D') {
-                    await import('../../URA_Reversa/src/server.js');
-                }
-            }
-            runFun2()
+            // async function runFun2() {
+            //     if (letter !== 'D') {
+            //         await import('../../URA_Reversa/src/server.js');
+            //     }
+            // }
+            // runFun2()
         });
 
         ret['ret'] = true
