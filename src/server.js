2376844e9d3522ba7456ee75142085dc05f2af70
Orlando <opseua@gmail.com>
2023-12-13 12:42:52 -0300

    Removido código em excesso

--- src/server.js
+++ src/server.js
@@ -8,9 +8,6 @@ async function server(inf) {
     //     else { process.on('uncaughtException', (errC) => errs(errC, ret)); process.on('unhandledRejection', (errC) => errs(errC, ret)) }
     // }
     try {
-        let infConfigStorage = { 'e': e, 'action': 'get', 'key': 'webSocket' };
-        let retConfigStorage = await configStorage(infConfigStorage); if (!retConfigStorage.ret) { return retConfigStorage } else { retConfigStorage = retConfigStorage.res }
-        let chatGptAiChatos = retConfigStorage.chatGptAiChatos
         let clients = new Set();
         let rooms = {};
 
@@ -76,50 +73,9 @@ async function server(inf) {
                         await log({ 'e': e, 'folder': 'JavaScript', 'path': `log.txt`, 'text': 'RESET' })
                         await log({ 'e': e, 'folder': 'JavaScript', 'path': `reset.js`, 'text': ' ' })
                     } else if (req.method == 'POST' && room.toLowerCase() == par4) {
-                        try {
-                            if (!message.length > 0) {
-                                throw new Error()
-                            } else {
-                                message = JSON.parse(message);
-                                if (!message.prompt) {
-                                    res.end(`INFORMAR O 'prompt'`)
-                                } else {
-                                    let network = message.network ? true : false; let infApi = {
-                                        'method': 'POST', 'url': chatGptAiChatos,
-                                        'headers': {
-                                            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"', 'accept': 'application/json, text/plain, */*',
-                                            'content-type': 'application/json', 'dnt': '1', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"',
-                                            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-                                            'origin': 'https://chat9.yqcloud.top', 'sec-fetch-site': 'cross-site', 'sec-fetch-mode': 'cors',
-                                            'sec-fetch-dest': 'empty', 'referer': 'https://chat9.yqcloud.top/', 'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,it;q=0.6'
-                                        }, 'body': { "prompt": message.prompt, "userId": "#/chat/1695782874949", "network": network, "system": "", "withoutContext": false, "stream": false }
-                                    };
-                                    let r = await api(infApi);
-                                    res.end(JSON.stringify({ 'ret': r.res.code == 200 ? true : false, 'res': r.res.body }))
-                                }
-                            }
-                        } catch (e) {
-                            res.end(`BODY INVALIDO`)
-                        }
+                        res.end(`CHAT GPT`)
                     } else if (req.method == 'POST' && room.toLowerCase() == par5) {
-                        let infApi, retApi
-                        infApi = { // ########## TYPE → json
-                            'method': 'POST', 'url': `https://ntfy.sh/`, 'headers': { 'accept-language': 'application/json' },
-                            'body': { 'Chave': 'aaa', 'Valor': 'bbb' }
-                        };
-                        infApi = { // ########## TYPE → text
-                            'method': 'POST', 'url': `https://ntfy.sh/`, 'headers': { 'content-type': 'text/plain;charset=UTF-8' },
-                            'body': '{"topic":"OPSEUA","message":"a"}'
-                        };
-                        let formData = new URLSearchParams(); // ########## TYPE → x-www-form-urlencoded
-                        formData.append('grant_type', 'client_credentials');
-                        formData.append('resource', 'https://graph.microsoft.com');
-                        infApi = {
-                            'method': 'POST', 'url': `https://ntfy.sh/`, 'headers': { 'Content-Type': 'application/x-www-form-urlencoded' },
-                            'body': formData.toString()
-                        };
-                        retApi = await api(infApi);
-                        res.end(`${req.method}: ERRO | NAO EXISTE '${room}'`)
+                        res.end(`API`)
                     } else if (!rooms[room]) {
                         res.end(`${req.method}: ERRO | NAO EXISTE '${room}'`)
                     } else if (message.length == 0) {
