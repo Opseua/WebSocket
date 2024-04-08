b43ce7a30e67535ee542a92f6b6e138c3a6b1457
Orlando <opseua@gmail.com>
2024-04-08 02:15:47 -0300

    'getPathNew' → 'getPath'

--- src/resources/@export.js
+++ src/resources/@export.js
@@ -2,7 +2,7 @@
 await import('../../../Chrome_Extension/src/resources/@functions.js');
 
 // DEFINIR → LETTER | ROOT | FUNCTION | PROJECT | FILE | LINE
-let retGetPathNew = await getPathNew({ 'e': new Error(), 'isFunction': false, 'devSlave': cng == 1 ? 'CHROME' : 'NODEJS' })
+let retGetPath = await getPath({ 'e': new Error(), 'isFunction': false, 'devSlave': cng == 1 ? 'CHROME' : 'NODEJS' })
 
 // FUNÇÕES DESSE PROJETO
 await import('./html.js');
