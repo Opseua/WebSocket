72a240b6646fb146f9a41a9722d2a0c9c2ca9363
Orlando <opseua@gmail.com>
2024-06-26 17:39:25 -0300

    Novo config EsLint

---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import { jsConfig } from '../../../ARQUIVOS/PROJETOS/Chrome_Extension/src/scripts/media/eslint.config.js'
+
+export default [...jsConfig]
--- src/.eslintrc.cjs
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    extends: 'D:/ARQUIVOS/PROJETOS/Chrome_Extension/.eslintrc.cjs'
-};
