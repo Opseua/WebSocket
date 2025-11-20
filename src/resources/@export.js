0a29ee5fe663363758e1e13eceefc3dd90b25597
Orlando <opseua@gmail.com>
2025-11-20 00:37:21 -0300

    Novo chrome.getProfileUserInfo

--- src/resources/@export.js
+++ src/resources/@export.js
@@ -1,8 +1,8 @@
 globalThis['eng'] = (typeof globalThis.alert !== 'undefined'); // [true] CHROME | [false] NODE
 
-// DEFINIR O 'devChildren' → [CHROME] EMAIL DO USUÁRIO | [NODE] PRIMEIRO ARQUIVO A SER EXECUTADO (NA MAIORIA DOS CASOS 'server')
+// DEFINIR O 'devChildren' → [CHROME] MODO ANÔNIMO → null / MODO NORMAL (SEM EMAIL) → false / MODO NORMAL (COM EMAIL) → email@gmail.com | [NODE] PRIMEIRO ARQUIVO A SER EXECUTADO (MAIORIA DOS CASOS 'server')
 let devC = new Error().stack.split('\n'); devC = devC[devC.length - 1]; let devChildren = devC.includes('.js:') ? devC.match(/\/([^/]+)\.[^/]+$/)[1] : false;
-if (eng) { devChildren = await new Promise((resolve) => { chrome.identity.getProfileUserInfo(function (u) { resolve(u.email); }); }); }
+if (eng) { devChildren = await new Promise((resolve) => { chrome.identity.getProfileUserInfo(u => { if (chrome.runtime.lastError) { resolve(null); return; } resolve(u.email || false); }); }); }
 
 // @functions
 await import(`../../../${process.env.fileChrome_Extension.split('PROJETOS\\')[1]}/src/resources/@functions.js`);
